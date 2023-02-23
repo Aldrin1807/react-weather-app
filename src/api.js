1a3f7a3e0b9159eaef28718f23f3d198f3ffff9b
Aldrin1807 <ai58914@ubt-uni.net>
2023-02-23 20:52:08 +0100

    completed

--- src/api.js
+++ src/api.js
@@ -2,12 +2,12 @@ export const geoApiOptions = {
     method: 'GET',
     url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
     headers: {
-      'X-RapidAPI-Key': 'cfc79d0c4amsh05187382b6b1750p1d3cb0jsnd11c7ac28fc4',
+      'X-RapidAPI-Key': '',
       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     },
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "d242e204f283043dab7add5b7195f9c0"; 
+  export const WEATHER_API_KEY = ""; 
   
